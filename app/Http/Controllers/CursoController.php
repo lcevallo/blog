0ab6a1be2d026dce6188ca6703fd95c4c10a01e9
Luis Alberto Cevallos Cavero <lcevallo@gmail.com>
2021-07-31 13:40:03 -0500

    Video 04
    Avanzando hasta el video 04

--- app/Http/Controllers/CursoController.php
+++ app/Http/Controllers/CursoController.php
@@ -8,16 +8,19 @@ class CursoController extends Controller
 {
     //metodo encargado de buscar la pagina principal
     public function index(){
-        return "Bienvenido a la pagina principal de cursos";
+        return view('cursos.index');
     }
 
     //metodo encargado de mostrar un formulario para que puedas crear
     public function create(){
-        return "En esta pagina podras crear un curso ";
+        return view('cursos.create');
     }
 
     //metodo encargado de mostrar un elemento
     public function show($curso){
-        return "Bienvenido al curso $curso";
+
+        compact('curso'); // ['curso' => $curso]
+        // return view('cursos.show',['curso'=>$curso]);
+        return view('cursos.show',compact('curso'));
     }
 }
---
+++ resources/views/cursos/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.plantilla')
+
+@section('title','Cursos')
+
+@section('content')
+<h2>Bienvenido a la pagina principal de cursos</h2>
+@endsection
